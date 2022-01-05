69bb5bf57990f209bd3ee30fb0f2951ddfea8340
Michael <mlmbufpi@gmail.com>
2022-01-05 14:40:16 -0300

    feat: faz requisicao ao viacep

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { TemplateFormModule } from './template-form/template-form.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataFormComponent } from './data-form/data-form.component';
@@ -14,7 +14,8 @@ import { DataFormComponent } from './data-form/data-form.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    TemplateFormModule
+    TemplateFormModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/operators';
 
 @Component({
   selector: 'app-template-form',
   templateUrl: './template-form.component.html',
   styleUrls: ['./template-form.component.css']
 })
+
+@Injectable()
 export class TemplateFormComponent implements OnInit {
 
   usuario:any = {
@@ -13,7 +17,7 @@ export class TemplateFormComponent implements OnInit {
     email: null
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
@@ -30,6 +34,20 @@ export class TemplateFormComponent implements OnInit {
   aplicaCSSdeErro(campo:any){
     return {
       'has-error': this.verificaValidTouched(campo)
+    }
+  }
+
+  consultarCEP(cep: any){
+    //Nova variável "cep" somente com dígitos.
+    cep = cep.replace(/\D/g, '');
+    if (cep != "") {
+      //Expressão regular para validar o CEP.
+      var validacep = /^[0-9]{8}$/;
+      //Valida o formato do CEP.
+      if(validacep.test(cep)) {
+        this.http.get("https://viacep.com.br/ws/"+ cep +"/json")
+        .subscribe(dado => console.log(dado))
+      }
     }
   }
 
