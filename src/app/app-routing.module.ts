ca5903d3c09b3b6d9b96ad921a58655d3e9a9aef
Michael <mlmbufpi@gmail.com>
2022-01-05 08:53:53 -0300

    feat: cria um form basico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { DataFormComponent } from './data-form/data-form.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'templateDriven', component: TemplateFormComponent },
+  { path: 'dataForm', component: DataFormComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'templateDriven' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
