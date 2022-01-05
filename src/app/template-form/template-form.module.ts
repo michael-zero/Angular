2fafc93b7b6f6ca3cb245b7376a68fe500030ef2
Michael <mlmbufpi@gmail.com>
2022-01-05 09:16:40 -0300

    feat: cria um modulo para o template-form

---
+++ src/app/template-form/template-form.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TemplateFormComponent } from './template-form.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    TemplateFormComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule
+  ]
+})
+export class TemplateFormModule { }
