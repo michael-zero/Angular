1bb217328798a6bf748ce69dcd2d50209f4ed648
Michael <mlmbufpi@gmail.com>
2022-01-06 15:39:44 -0300

    feat: uso do select com para setar OBJETOS

--- src/app/shared/services/dropdown.service.ts
+++ src/app/shared/services/dropdown.service.ts
@@ -13,4 +13,29 @@ export class DropdownService {
     return this.http.get<EstadoBr[]>('../../../assets/dados/estadosbr.json')
     .pipe()
   }
+
+  getCargos() {
+    return [
+      { nome: 'Dev', nivel: 'Junior', desc: 'Dev Jr' },
+      { nome: 'Dev', nivel: 'Pleno', desc: 'Dev Pl' },
+      { nome: 'Dev', nivel: 'Senior', desc: 'Dev Sr' }
+    ];
+  }
+
+  getTecnologias() {
+
+    return [
+      { nome: 'java', desc: 'Java' },
+      { nome: 'javascript', desc: 'JavaScript' },
+      { nome: 'php', desc: 'PHP' },
+      { nome: 'ruby', desc: 'Ruby' }
+    ];
+  }
+
+  getNewsletter() {
+    return [
+      { valor: 's', desc: 'Sim' },
+      { valor: 'n', desc: 'Não' }
+    ];
+  }
 }
